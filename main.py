d05e2a08deaec6c7e6638736df688bd53a59ea73
Tazzina Afroze Hassan <tazzinaafroze98@gmail.com>
2020-07-17 19:02:18 +0600

    Initial Build

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%I:%M:%S %p')
+
+    label.config(text=string)
+    label.after(2000,time)
+
+label = Label(root, font=("ds-digital", 80), background = "navy blue", foreground = "white")
+label.pack(anchor='center')
+
+time()
+
+mainloop()
